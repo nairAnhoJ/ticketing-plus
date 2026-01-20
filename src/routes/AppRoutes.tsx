7d7786cbd43e5b1136daa0d9d82cbc0e6af94176
unknown <malondrasarian@gmail.com>
2026-01-20 14:55:20 +0800

    add login route and connect login page

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,11 +1,18 @@
 import { Route, Routes } from 'react-router-dom'
+
+import LoginPage from '../features/auth/LoginPage'
+
 import HomeIndex from '../features/home/HomeIndex'
 
 function AppRoutes() {
   return (
     <>
       <Routes>
+
           <Route path='/' element={<HomeIndex />}></Route>
+
+          
+          <Route path='/login' element={<LoginPage />}></Route>
       </Routes>
     </>
   )
