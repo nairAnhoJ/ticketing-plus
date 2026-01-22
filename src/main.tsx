010da73d2289425ed944cc61c2ac4dd45200aed6
unknown <malondrasarian@gmail.com>
2026-01-22 19:13:53 +0800

    Provider added and update css in index

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import AppRoutes from './routes/AppRoutes'
+import { Provider } from 'react-redux'
+import { store } from './app/store'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>,
 )
