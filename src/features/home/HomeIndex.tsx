ea9a6d76d80e3c0c6004db42e4cb6f661e18fe07
unknown <malondrasarian@gmail.com>
2026-01-26 14:15:49 +0800

    home page template - done

--- src/features/home/HomeIndex.tsx
+++ src/features/home/HomeIndex.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { useAppSelector } from "../../app/hooks";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 const HomeIndex = () => {
@@ -15,7 +15,7 @@ const HomeIndex = () => {
 
     return (
         <>
-            <div className="w-screen h-dvh flex flex-col bg-[#212121]">
+            <div className="w-screen h-screen overflow-hidden flex flex-col bg-[#212121]">
                 <div className="w-full h-16 py-2.25 px-2.5 text-white flex justify-center items-end">
                     <div className="w-full h-full flex items-center">
                         <img src="/default-avatar.jpg" alt="avatar" className="h-full rounded-full border-2 border-neutral-500" />
@@ -28,11 +28,14 @@ const HomeIndex = () => {
                             </button>
                             <p className="text-xs leading-3">{JSON.parse(user).department}</p>
                             {
-                                showProfileMenu && 
-                                <div className="absolute flex flex-col top-6.5 -left-1 rounded p-1 bg-[#454545]">
-                                    <button className="py-2 whitespace-nowrap border-b border-[#808080] px-5">Change Avatar</button>
-                                    <button onClick={handleLogout} className="py-2">Logout</button>
-                                </div>
+                                showProfileMenu &&
+                                <>
+                                    <div onClick={()=>setShowProfileMenu(!showProfileMenu)} className="fixed top-0 left-0 w-screen h-screen z-1"></div>
+                                    <div className="absolute flex flex-col top-6.5 -left-1 rounded p-1 bg-[#454545] z-2">
+                                        <button className="py-2 whitespace-nowrap border-b border-[#808080] px-5">Change Avatar</button>
+                                        <button onClick={handleLogout} className="py-2">Logout</button>
+                                    </div>
+                                </>
                             }
                         </div>
                     </div>
@@ -50,26 +53,28 @@ const HomeIndex = () => {
                     </div>
                     <div className="bg-amber-400 w-full rounded-xl flex flex-col justify-center items-center">
                         <p className="font-bold text-lg leading-5">99</p>
-                        <p className="text-xs leading-3.5">Ongoing</p>
+                        <p className="text-xs leading-3.5">In-Progress</p>
                     </div>
                     <div className="bg-emerald-400 w-full rounded-xl flex flex-col justify-center items-center">
                         <p className="font-bold text-lg leading-5">99</p>
                         <p className="text-xs leading-3.5">Needs Feedback</p>
                     </div>
                 </div>
-                <div className="flex-1 h-[calc(100%-144px)] w-full bg-neutral-100 rounded-t-xl py-3 text-neutral-600">
+                <div className="flex-1 h-[calc(100%-132px)] w-full bg-neutral-100 rounded-t-xl pt-3 text-neutral-600">
                     {/* Search */}
                     <div className="w-full h-10 relative px-3">
                         <input type="text" className="w-full h-10 border border-neutral-600 rounded-lg bg-white pl-8.5 focus:outline-0"/>
-                        <svg xmlns="http://www.w3.org/2000/svg" className="w-7 absolute top-1.5 left-7.5" viewBox="0 -960 960 960" fill="currentColor">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="w-7 absolute top-1.5 left-4.5" viewBox="0 -960 960 960" fill="currentColor">
                             <path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z"/>
                         </svg>
                     </div>
 
+                    
+                    <h1 className="text-xs font-bold text-[#808080] px-3 pt-3 pb-1">TICKETS</h1>
+
                     {/* List */}
-                    <div className="w-full h-[calc(100%-40px)] overflow-x-hidden overflow-y-auto mt-3 border-neutral-400">
-                        <h1 className="text-xs font-bold text-[#808080] px-3 pb-1">TICKETS</h1>
-                        <Link to="/tickets/id" className="w-full border-y border-neutral-400 p-2 flex flex-col">
+                    <div className="w-full h-[calc(100%-72px)] overflow-x-hidden border-t overflow-y-auto border-neutral-400">
+                        <Link to="/tickets/id" className="w-full border-b border-neutral-400 p-2 flex flex-col">
                             <div className="flex justify-between items-center">
                                 <h1 className="font-bold">USER NA NAG-REQUEST</h1>
                                 <p className="text-xs">Jan 24, 2026 07:25 PM</p>
@@ -77,7 +82,7 @@ const HomeIndex = () => {
                             <h2 className="font-medium text-sm">SUBJECT NG TICKET</h2>
                             <p className="text-xs truncate">Ito naman yung description ng ticket Ito naman yung description ng ticket Ito naman yung description ng ticket Ito naman yung description ng ticket </p>
                         </Link>
-                        <Link to="/tickets/id" className="w-full border-y border-neutral-400 p-2 flex flex-col">
+                        <Link to="/tickets/id" className="w-full border-b border-neutral-400 p-2 flex flex-col">
                             <div className="flex justify-between items-end">
                                 <h1 className="font-bold">USER NA NAG-REQUEST</h1>
                                 <p className="text-xs">Jan 24, 2026 07:25 PM</p>
