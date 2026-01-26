86aa5fe7a7f0a1ceadf526dad3ebb60c74bee89a
unknown <malondrasarian@gmail.com>
2026-01-26 16:16:57 +0800

    home page template for mobile - done

--- src/features/home/HomeIndex.tsx
+++ src/features/home/HomeIndex.tsx
@@ -15,7 +15,7 @@ const HomeIndex = () => {
 
     return (
         <>
-            <div className="w-screen h-screen overflow-hidden flex flex-col bg-[#212121]">
+            <div className="lg:hidden w-screen h-screen overflow-hidden flex flex-col bg-[#212121]">
                 <div className="w-full h-16 py-2.25 px-2.5 text-white flex justify-center items-end">
                     <div className="w-full h-full flex items-center">
                         <img src="/default-avatar.jpg" alt="avatar" className="h-full rounded-full border-2 border-neutral-500" />
@@ -46,7 +46,7 @@ const HomeIndex = () => {
                         </svg>
                     </Link>
                 </div>
-                <div className="w-full h-15 flex gap-x-2 pb-2 px-2.5 mb-2">
+                <div className="w-full sm:w-112.5 h-15 flex gap-x-2 pb-2 px-2.5 mb-2 mx-auto">
                     <div className="bg-red-400 w-full rounded-xl flex flex-col justify-center items-center">
                         <p className="font-bold text-lg leading-5">99</p>
                         <p className="text-xs leading-3.5">Pending</p>
@@ -63,7 +63,7 @@ const HomeIndex = () => {
                 <div className="flex-1 h-[calc(100%-132px)] w-full bg-neutral-100 rounded-t-xl pt-3 text-neutral-600">
                     {/* Search */}
                     <div className="w-full h-10 relative px-3">
-                        <input type="text" className="w-full h-10 border border-neutral-600 rounded-lg bg-white pl-8.5 focus:outline-0"/>
+                        <input type="text" className="w-full h-10 border border-neutral-600 rounded-xl bg-white pl-8.5 focus:outline-0"/>
                         <svg xmlns="http://www.w3.org/2000/svg" className="w-7 absolute top-1.5 left-4.5" viewBox="0 -960 960 960" fill="currentColor">
                             <path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z"/>
                         </svg>
