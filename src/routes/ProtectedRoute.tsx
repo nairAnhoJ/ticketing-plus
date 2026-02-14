46782734b1e320a2fffb7739a5a9bea8c0b09ba1
unknown <malondrasarian@gmail.com>
2026-02-14 10:38:45 +0800

    return to login if user is not valid

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,13 +1,34 @@
 import { useSelector } from "react-redux"
 import type { RootState } from "../app/store"
 import { Navigate, Outlet } from "react-router-dom"
+import config from "../config/config"
+
 
 const ProtectedRoute = () => {
     const token = useSelector((state: RootState) => state.auth.token)
 
+    const isValid = async () => {
+        try {
+            const valid = await config.get('/auth/is-valid');
+            return valid.data;
+        } catch (error: any) {
+            return error.response;
+        }
+    }
+
+    isValid().then((res) => {
+        if(res.status === 401 && res.data.message === "Unauthorized"){
+            localStorage.removeItem("token");
+            localStorage.removeItem("user");
+            window.location.href = "/login";
+        }
+    })
+
     if(!token){
         return <Navigate to="/login" replace />
     }
+
+
 
     return <Outlet />
 }
